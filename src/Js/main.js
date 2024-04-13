084026830f6d249e3f8e61f9f89f17a4b29a12b1
vishalkrkamat <vishalkrkamat7@gmail.com>
2024-04-13 23:27:34 +0530

    raw changes in project section

--- src/Js/main.js
+++ src/Js/main.js
@@ -7,6 +7,10 @@ import css from "../assets/css_img.svg";
 import html from "../assets/html_img.svg";
 import linux from "../assets/linux_img.svg";
 import git from "../assets/git_img.svg";
+import pic1 from "../assets/pic1.png";
+import pic2 from "../assets/pic2.png";
+import pic3 from "../assets/pic3.png";
+import pic4 from "../assets/pic4.png";
 
 const profilepic = document.getElementById("pro_image");
 profilepic.src = profile;
@@ -25,3 +29,15 @@ linux_img.src = linux;
 
 const gitimg = document.getElementById("git");
 gitimg.src = git;
+
+const pict_1 = document.getElementById("pic1");
+pict_1.src = pic1;
+
+const pict_2 = document.getElementById("pic2");
+pict_2.src = pic2;
+
+const pict_3 = document.getElementById("pic3");
+pict_3.src = pic3;
+
+const pict_4 = document.getElementById("pic4");
+pict_4.src = pic4;
